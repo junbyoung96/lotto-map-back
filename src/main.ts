d933bf72fac1adc4f369fb2ca8500e16a7882c7c
junbyoung96 <junbyoung96@gmail.com>
2024-05-02 22:07:42 +0900

    second commit

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:3000',  // 허용할 오리진
+    origin: '*',  // 허용할 오리진
     methods: 'GET,POST',           // 허용할 HTTP 메소드
     allowedHeaders: 'Content-Type,Authorization', // 허용할 헤더
     credentials: true,             // 쿠키를 포함한 요청 허용 여부
